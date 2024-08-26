041ed24546db4a2e19fbb4591f59877782deb844
Gildevan Ferreira <gildevanferrer@gmail.com>
2024-08-25 22:31:46 -0300

    feat: validation tokens for the requested

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,33 @@
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api'
-})
+});
+
+api.interceptors.request.use(
+    config => {
+        if (config.url === '/auth/login') {
+            return config;
+        }
+
+        
+        const authData = JSON.parse(localStorage.getItem('authorization'));
+
+        if (authData && authData.token) {
+            config.headers.Authorization = `Bearer ${authData.token}`;
+            return config;
+        }
+
+        localStorage.removeItem('user');
+        localStorage.removeItem('menu');
+        window.location.href = '/login';
+
+        return Promise.reject(new Error("Token não encontrado, usuário deslogado."));
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
